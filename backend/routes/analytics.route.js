4462717c690a2e69ba6f3e73a3d4ddae5b0b4e11
YadavSaurav64 <yadavsaurav2801@gmail.com>
2026-01-07 17:40:53 +0530

    Initial uploads

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { protectRoute,adminRoute } from "../middleware/auth.middleware.js";
+import { getAnalyticsData,getDailySalesData } from "../controllers/analytics.controller.js";
+
+const router = express.Router();
+
+router.get("/",protectRoute,adminRoute,async(req,res)=>{
+  try {
+    const analyticData =await getAnalyticsData();
+    const endDate=new Date();
+    const startDate =new Date(endDate.getTime()-7*24*60*60*1000); // 7 days ago
+
+    const dailySalesData =await getDailySalesData(startDate,endDate);
+
+    res.json({
+      analyticData,
+      dailySalesData,
+    });
+  } catch (error) {
+    console.log("Error in analytics route",error.message);
+    res.status(500).json({message:"server error",error:error.message});
+  }
+});
+
+export default router;
---
+++ backend/routes/payment.route.js
@@ -0,0 +1,10 @@
+import {protectRoute} from '../middleware/auth.middleware.js';
+import { checkoutSuccess,createCheckoutSession } from '../controllers/payment.controller.js';
+import express from "express";
+
+const router = express.Router();
+
+router.post("/create-checkout-session",protectRoute,createCheckoutSession);
+router.post("/checkout-success",protectRoute,checkoutSuccess);
+
+export default router;
